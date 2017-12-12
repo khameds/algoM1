bb47c11e8fca9e8d06b8535b614a5560c9486389
KHAMED Smaïl <khameds@gmail.com>
2017-12-12 18:29:11 +0100

    [feat] define getters and setters for rec algos

--- BranchAndBoundApproach.java
+++ BranchAndBoundApproach.java
@@ -2,6 +2,23 @@
 public class BranchAndBoundApproach
 {
 	private String word1;
+
+	public String getWord1() {
+		return word1;
+	}
+
+	public void setWord1(String word1) {
+		this.word1 = word1;
+	}
+
+	public String getWord2() {
+		return word2;
+	}
+
+	public void setWord2(String word2) {
+		this.word2 = word2;
+	}
+
 	private String word2;
 	public int bound;
 
